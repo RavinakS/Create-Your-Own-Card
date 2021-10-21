3aa2dc78b38d31e998e4d62d30352f29f411d89c
RavinakS <ravinasingh697@gmail.com>
2021-10-21 11:19:19 +0530

    the login route is working fine, need to add password encryption and authentication using JWT

--- controller/logIn.js
+++ controller/logIn.js
@@ -4,7 +4,7 @@ const login = (req, res)=>{
     let user_info = req.body;
     obj_keys = Object.keys(user_info);
     user_id = obj_keys[0]
-    users_detail_tbl.login(user_info.user_id)
+    users_detail_tbl.login(user_info[user_id])
     .then((response)=>{
         res.send(response);
     })
