29e4d8883eaa615f654867e6d62db07d9d51e632
RavinakS <ravinasingh697@gmail.com>
2021-10-21 17:40:55 +0530

    schema validation

--- controller/schema_validation.js
+++ controller/schema_validation.js
@@ -1,10 +1,83 @@
 const Joi = require('joi');
 
-const schema = Joi.object({
+const test1 = Joi.object({
     userName: Joi.string().alphanum().min(3).max(15).required(),
     email: Joi.string().email().required(),
-    phoneNum: Joi.string().length(10).pattern(/^[0-9]+$/).required()
+    phoneNum: Joi.string().length(10).pattern(/^\d+$/).required(),
+    Dob: Joi.date().raw()
 })
 
+// function termAndConditions(t_and_c){
+//     if(t_and_c === true){
+//         return 'excepted';
+//     }else if(t_and_c === false){
+//         return "notExcepted";
+//     }else{
+//         return "invalid";
+//     }
+// }
 
-module.exports = {passwordValidation, termAndConditions}
+module.exports = {test1}
+
+
+
+
+// function isNumExist(password){
+//     let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//     var index = 0;
+//     var checker = 0
+//     for(index; index<password.length; index++){
+//         try{
+//             var paswrdNum = parseInt(password[index], 10);
+//             if(numbers.includes(paswrdNum)){
+//                 checker ++;
+//             }
+//         }catch{
+//             continue;
+//         }
+//     }
+
+//     if(checker > 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// function isCharExist(password){
+//     let characters = 'abcdefghijklmnopqrstuvwxyz';
+//     char = 0;
+//     countChar = 0;
+//     for(char; char<password.length; char++){
+//         if(characters.includes(password[char])){
+//             countChar = countChar + 1;
+//         }
+//     }
+//     if(countChar>=6){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// function passwordValidation(password){
+//     if(password.includes("@") || password.includes("#")){
+//         containNum = isNumExist(password);
+//         if(containNum === true){
+//             containChar = isCharExist(password);
+//             if(containChar === true){
+//                 console.log('You are logged in successfully');
+//                 return 'valid';
+//             }else{
+//                 console.log("Password should have minimum 6 characters");
+//                 return 'unvalid';
+//             }
+//         }else{
+//             console.log("Atleas password should contain one number.");
+//             return 'unvalid';
+//         }
+//     }else{
+//         console.log("Atleas password should contain one special character.");
+//         return 'unvalid';
+//     }
+// }
--- controller/signUp.js
+++ controller/signUp.js
@@ -1,36 +1,49 @@
 const users_detail_tbl = require('../model/signUp');
-const validations = require('./schema_validation');
+const test = require('./schema_validation');
+const Joi = require('joi');
 
 const createAccount = (req, res) =>{
-    if(validations.passwordValidation(req.body.password) === 'valid'){
-        if(validations.termAndConditions(req.body.T_and_C) === 'excepted'){
-            let {profilePic_url, userName, email, phoneNum, password, gender, Dob, qualification, T_and_C} = req.body;
-            let user_details = {
-                "profilePic_url": profilePic_url,
-                "userName": userName,
-                "email": email,
-                "phoneNum": phoneNum,
-                "password": password,
-                "gender": gender,
-                "Dob": Dob,
-                "qualification": qualification,
-                "T_and_C": T_and_C
-            }
-            users_detail_tbl.createAccount(user_details)
-            .then((response)=>{
-                res.send('Account is successfully created!!');
-            })
-            .catch((err)=>{
-                res.send(err);
-            })
-        }else if(validations.termAndConditions(req.body.T_and_C) === 'notExcepted'){
-            res.send("You need to except the terms and conditions to create account.");
-        }else{
-            res.send(`please type "true" if you are agree with our terms and conditions or else type "false".`);
-        }
-    }else{
-        res.send('Please provide a strong password!');
+    //error handling
+    let {profilePic_url, userName, email, phoneNum, password, gender, Dob, qualification, T_and_C} = req.body;
+    let user_details = {
+        "profilePic_url": profilePic_url,
+        "userName": userName,
+        "email": email,
+        "phoneNum": phoneNum,
+        "password": password,
+        "gender": gender,
+        "Dob": Dob,
+        "qualification": qualification,
+        "T_and_C": T_and_C
     }
+
+    let validate = test.test1.validate(req.body)
+    if(validate.error){
+        res.send(error);
+        // res.status(400).send(validation.error.details[0].message);
+    }else{
+        users_detail_tbl.createAccount(user_details)
+        .then((response)=>{
+            res.send('Account is successfully created!!');
+        })
+        .catch((err)=>{
+            res.send(err);
+        })
+    }
+    // Joi.validate(user_details, schema, (err, result)=>{
+    //     if(err){
+    //         // res.status(400).send(err.details[0].message)
+    //         res.send(err);
+    //     }else{
+    //         users_detail_tbl.createAccount(user_details)
+    //         .then((response)=>{
+    //             res.send("Account is successfully created!!");
+    //         })
+    //         .catch((err)=>{
+    //             console.log(err);
+    //         })
+    //     }
+    // })
 }
 
 module.exports = createAccount;
