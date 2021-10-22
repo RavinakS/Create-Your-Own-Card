67c01ef521831b447ccf9a7702ed835d69aca921
RavinakS <ravinasingh697@gmail.com>
2021-10-22 17:58:49 +0530

    schema validation is done

--- controller/schema_validation.js
+++ controller/schema_validation.js
@@ -1,11 +1,18 @@
 const Joi = require('joi');
 
-const test1 = Joi.object({
+const validations = Joi.object({
+    profilePic_url:Joi.string(),
     userName: Joi.string().alphanum().min(3).max(15).required(),
     email: Joi.string().email().required(),
     phoneNum: Joi.string().length(10).pattern(/^\d+$/).required(),
-    Dob: Joi.date().raw()
+    password: Joi.string().min(4).max(10).required(),
+    gender: Joi.string(),
+    Dob: Joi.date().raw(),
+    qualification: Joi.string(),
+    T_and_C: Joi.bool()
 })
+
+module.exports = {validations}
 
 // function termAndConditions(t_and_c){
 //     if(t_and_c === true){
@@ -14,70 +21,5 @@ const test1 = Joi.object({
 //         return "notExcepted";
 //     }else{
 //         return "invalid";
-//     }
-// }
-
-module.exports = {test1}
-
-
-
-
-// function isNumExist(password){
-//     let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//     var index = 0;
-//     var checker = 0
-//     for(index; index<password.length; index++){
-//         try{
-//             var paswrdNum = parseInt(password[index], 10);
-//             if(numbers.includes(paswrdNum)){
-//                 checker ++;
-//             }
-//         }catch{
-//             continue;
-//         }
-//     }
-
-//     if(checker > 0){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-
-// function isCharExist(password){
-//     let characters = 'abcdefghijklmnopqrstuvwxyz';
-//     char = 0;
-//     countChar = 0;
-//     for(char; char<password.length; char++){
-//         if(characters.includes(password[char])){
-//             countChar = countChar + 1;
-//         }
-//     }
-//     if(countChar>=6){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-
-// function passwordValidation(password){
-//     if(password.includes("@") || password.includes("#")){
-//         containNum = isNumExist(password);
-//         if(containNum === true){
-//             containChar = isCharExist(password);
-//             if(containChar === true){
-//                 console.log('You are logged in successfully');
-//                 return 'valid';
-//             }else{
-//                 console.log("Password should have minimum 6 characters");
-//                 return 'unvalid';
-//             }
-//         }else{
-//             console.log("Atleas password should contain one number.");
-//             return 'unvalid';
-//         }
-//     }else{
-//         console.log("Atleas password should contain one special character.");
-//         return 'unvalid';
 //     }
 // }
--- controller/signUp.js
+++ controller/signUp.js
@@ -1,5 +1,5 @@
 const users_detail_tbl = require('../model/signUp');
-const test = require('./schema_validation');
+const schema = require('./schema_validation');
 const Joi = require('joi');
 
 const createAccount = (req, res) =>{
@@ -17,10 +17,10 @@ const createAccount = (req, res) =>{
         "T_and_C": T_and_C
     }
 
-    let validate = test.test1.validate(req.body)
-    if(validate.error){
+    let {error, result} = schema.validations.validate(user_details)
+    if(error){
         res.send(error);
-        // res.status(400).send(validation.error.details[0].message);
+        // res.send(error.details[0].message);
     }else{
         users_detail_tbl.createAccount(user_details)
         .then((response)=>{
@@ -30,20 +30,6 @@ const createAccount = (req, res) =>{
             res.send(err);
         })
     }
-    // Joi.validate(user_details, schema, (err, result)=>{
-    //     if(err){
-    //         // res.status(400).send(err.details[0].message)
-    //         res.send(err);
-    //     }else{
-    //         users_detail_tbl.createAccount(user_details)
-    //         .then((response)=>{
-    //             res.send("Account is successfully created!!");
-    //         })
-    //         .catch((err)=>{
-    //             console.log(err);
-    //         })
-    //     }
-    // })
 }
 
 module.exports = createAccount;
