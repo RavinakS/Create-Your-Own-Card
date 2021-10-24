d83748bc94d0b8c862a48ddf74636e22151e29c3
RavinakS <ravinasingh697@gmail.com>
2021-10-25 03:34:24 +0530

    <<<---->>>

--- Connection/dbConnection.js
+++ Connection/dbConnection.js
@@ -1,9 +1,14 @@
+const host = process.env.host;
+const user = process.env.mysqlUser;
+const password = process.env.mysqlUserPassword;
+
+
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: "ravina@123",
+        host: host,
+        user: user,
+        password: password,
         database: 'infistack'
     }
 });
